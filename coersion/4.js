6f8d1dc8f09ef823cb3161538357dc1ff979936f
Stan Muszel <stanislaw.muszel@gmail.com>
2019-03-25 18:23:29 +0100

    array

--- coersion/4.js
+++ coersion/4.js
@@ -1,9 +1,14 @@
 [
     typeof NaN,
     typeof [],
+    typeof false,
+    typeof Infinity,
     typeof null,
     typeof Symbol(),
     typeof (() => { }),
     typeof '',
+    typeof delete 'x',
+    typeof Error(),
+    typeof Date(),
     typeof undefined
-].filter('object').length;
+]
---
+++ other/array.js
@@ -0,0 +1,24 @@
+new Array(10).forEach((_, ix) => console.log(ix));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const xs = new Array(5).fill([]);
+xs[0].push(1);
+xs.flat();
